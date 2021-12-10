795e7dab0a7bf75e2ae1d51863116da17dc76ff6
fab-scm <fabian.schumacher@uni-muenster.de>
2021-12-10 12:20:32 +0100

    Rasterdaten Ladebedarf und r_script

--- R/rasterize.R
+++ R/rasterize.R
@@ -4,39 +4,46 @@ library(stars)
 library(mapview)
 library(ggplot2)
 library(rgeos)
+library(rstudioapi)
+library(utils)
 
 rm(list = ls())
-
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 setwd("/media/fabian-s/3465-6433/Studium/5. Semester/Projekt Aufladen in Münster")
 
-ladebedarf = read_sf("./Daten StandortTOOL Szenarien/NRW_Münster_2022.geojson")
+ladebedarf <- read_sf("./Daten StandortTOOL Szenarien/NRW_Münster_2022.geojson")
+siedlungsflächen <- read_sf("./Daten StandortTOOL Szenarien/Siedlungsflächen Münster.gpkg")
 
-ladebedarf = st_transform(ladebedarf, st_crs("EPSG:32632"))
-ladebedarf
+
+ladebedarf <- st_transform(ladebedarf, st_crs("EPSG:32632"))
+siedlungsflächen <- st_transform(siedlungsflächen, st_crs("EPSG:32632"))
+
+ladebedarf_intersected = st_intersection(ladebedarf, siedlungsflächen)
+
+
+mapview(ladebedarf_intersected)
+plot(ladebedarf_intersected)
 
 
 
 bbox = bbox2SP(5767767.6, 5745727.6 , 397791.9, 413821.9, bbox=NA, proj4string=CRS("+init=epsg:32632"))
+
 bbox = st_bbox(bbox)
 bbox
 
 
-plot(ladebedarf)
 
+x = st_rasterize(ladebedarf_intersected, st_as_stars(bbox, nx = 1603, ny = 2204, values = NA_real_))
 
-stars_bbox = st_as_stars(bbox, nx = 1603, ny = 2204, values = NA_real_)
-
-x = st_rasterize(ladebedarf, st_as_stars(bbox, nx = 1603, ny = 2204, values = NA_real_))
-
-# y = st_rasterize(ladebedarf, guess_raster(ladebedarf))
-
-
-mapview(muenster_ladebedarfs$muenster)
 
 class(x)
 
 write_stars(x, "muenster.tif")
 
-muenster_ladebedarfs = stack("muenster.tif")
+muenster_ladebedarf <- raster("muenster.tif")
 
-muenster_ladebedarfs
+muenster_ladebedarf
+
+mapview(muenster_ladebedarf)
+
+
