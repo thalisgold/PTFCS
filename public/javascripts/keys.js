7825f8a62072e2add9babd4420c7bd0ba18836e8
fab-scm <fabian.schumacher@uni-muenster.de>
2021-12-13 13:34:51 +0100

    delete unnecessary files

--- public/javascripts/keys.js
+++
@@ -1,3 +0,0 @@
-// API Keys
-const mapboxToken = 'pk.eyJ1IjoiZmFiLXNjbSIsImEiOiJja3Ayc3FsMHcwOHJyMnVta3NnZnc2c29yIn0.luDg7rI91KhbPisDYcIxYg';
-const openweatherAPIKey = "97bc8bb7723c267f11e85434445965a9";
