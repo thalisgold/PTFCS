596044fc74e7ad1c0ab4794cb42444468677eb82
GitLiliana <l_gits01@uni-muenster.de>
2022-01-14 13:34:50 +0100

    Update Umrechnung Gievenbeck

--- R/UmrechnungGievenbeck.R
+++ R/UmrechnungGievenbeck.R
@@ -6,25 +6,28 @@ library(raster)
 library(sf)
 
 # set working directory
-setwd("C:/Users/lgits/sciebo/Uni_Geoinfo/A2_Studienprojekt_Aufladen/Projekt/PTFCS/Daten StandortTOOL Szenarien")
+setwd("C:/Users/lgits/sciebo/Uni_Geoinfo/A2_Studienprojekt_Aufladen/Projekt/PTFCS/Daten StandortTOOL Szenarien/gpkg und andere EPSG codes")
 
 # load Gievenbeck
-gievenbeck <- st_read("GievenbeckFlaeche.gpkg")
+gievenbeck <- st_read("Gievenbeck2022_Flaeche.gpkg")
 
 # Berechung des Gesamtpotenzials für Gievenbeck
 gesamtpotenzial <- 0
 for (i in 1:19) {
-  numberPixels <- ((gievenbeck$Flaeche[i])/100)  # Fläche durch 100 teilen, da ein Pixel 10mx10m groß ist
+  numberPixels <- ((gievenbeck$Flaeche[i])/100)  # Flaeche durch 100 teilen, da ein Pixel 10mx10m groß ist
   potenzial <- numberPixels * (gievenbeck$Potenzial[i])
   gesamtpotenzial <- (gesamtpotenzial + potenzial)
 }
 gesamtpotenzial
 
 # Umrechnung in Stunden pro Wert 1 des StandortTOOls pro Pixel (klein/eigene)
-bedarf_std <- 136   # hier den ermittelten Bedarf an Ladestunden für Gievenbeck eintragen
-wert1 <- bedarf_std/gesamtpotenzial
-wert1
-# wert1 gibt nun den Bedarf an Ladestunden für eines unserer Pixel an,
-# wenn im StandortTOOL für den Ladebedarf der Wert 1 angegeben ist.
-# Für den Wert 2 des StandortTOOL ergibt sich damit: wert2 = 2 * wert1
-# Für die weiteren Werte analog.
+bedarf_std <- 358.75   # ermittelter Bedarf an Ladestunden für Gievenbeck
+                    # (übernommen aus dem Projekt von Dominik Zubel + Dominik Waldmann)
+wert1_Std <- bedarf_std/gesamtpotenzial
+wert1_Std
+wert1_min <- wert1_Std*60
+wert1_min
+# wert1_Std/wert1_min gibt nun den Bedarf an Ladestunden/-minuten für eines unserer Pixel an,
+# wenn im StandortTOOL fuer den Ladebedarf der Wert 1 angegeben ist.
+# Fuer den Wert 2 des StandortTOOL ergibt sich damit: wert2 = 2 * wert1
+# Fuer die weiteren Werte analog.
